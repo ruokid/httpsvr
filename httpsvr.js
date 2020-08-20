3efeb5922ee72b5c462d3a819e3c6f10a6b460a4
cx <imdevcx@gmail.com>
2020-08-20 12:21:09 +0800

    Update httpsvr.js

--- httpsvr.js
+++ httpsvr.js
@@ -16,8 +16,9 @@ const RIB = {'': {}};
  * @param path {String} 请求地址路径部分，不包括协议、域名和查询参数
  * @param methods {String|String[]} 允许的请求方法，默认接受所有(http.METHODS)，需要大写字母
  * @param handler {Function} 请求处理函数(IncomingMessage, ServerResponse)
+ * @param options {*} 可选项
  */
-exports.route = function(path, methods, handler) {
+exports.route = function(path, methods, handler, options={}) {
   switch (typeof methods) {
     case 'function':
       handler = methods;
@@ -39,12 +40,6 @@ exports.route = function(path, methods, handler) {
     path = '/' + path;
   }
 
-  // 直接储存
-  // RIB[path] = {
-  //   methods: methods.map((m) => m.toUpperCase()),
-  //   handler: handler
-  // }
-
   // 分隔path，然后按键储存
   let rib = RIB;
   path.split('/').forEach((v) => {
@@ -58,7 +53,12 @@ exports.route = function(path, methods, handler) {
       rib = v === '' ? rib[v]['*'] : rib[v];
     }
   });
-  rib[''] = { path: path, methods: methods, handler: handler };
+
+  rib[''] = Object.assign(options, {
+    path: path,
+    methods: methods,
+    handler: handler
+  });
 }
 
 /**
@@ -136,32 +136,9 @@ exports.createServer = function(options={}) {
       );
     });
 
-    request.qs = qs; //附加上解析好的查询参数对象(QueryString)
+    request.$ = qs; //附加上解析好的查询参数对象(QueryString)
     response.setHeader('Content-Type', 'text/plain'); //默认返回文本内容
     response.setHeader('Server', server); //自定义服务器名称
-
-    // let rib = RIB[pathname];
-    // if (rib) {
-    //   if (rib.methods.length === 0 || rib.methods.indexOf(request.method) >= 0) {
-    //     rib.handler(request, response); //执行handler
-    //   }
-    //   else {
-    //     response.writeHead(405, {
-    //       Allow: rib.methods.join(',')
-    //     });
-    //     response.end();
-    //   }
-    // }
-    // else if (fs.existsSync(filename)) {
-    //   fextname = fextname[0] === '.' ? fextname.substr(1) : fextname;
-    //   response.setHeader('Content-Type', mime[fextname] || 'text/plain');
-    //   response.setHeader('Content-Length', fs.statSync(filename).size);
-    //   fs.createReadStream(filename).pipe(response);
-    // }
-    // else {
-    //   response.writeHead(404);
-    //   response.end();
-    // }
 
     let pns = [];
     let rib = RIB;
@@ -186,14 +163,25 @@ exports.createServer = function(options={}) {
 
     if (foundPath && rib.handler) {
       if (rib.methods.includes(request.method)) {
-        request['$'] = {};
+        request.$path = {}; //路径参数
         rib.path.split('/')
           .filter((v) => v.length > 0 && v.startsWith('{') && v.endsWith('}'))
           .forEach((v, i) => {
-            request['$'][v.substring(1, v.length - 1)] = pns[i];
+            request.$path[v.substring(1, v.length - 1)] = pns[i];
           });
+
         try {
-          rib.handler(request, response); //执行handler
+          if (rib.nodata) {
+            rib.handler(request, response); //自行接收数据
+          }
+          else {
+            request.data = Buffer.from([]);
+            request.on('data', (chunk) => {
+              request.data = Buffer.concat([request.data, chunk], request.data.length + chunk.length);
+            }).on('end', () => {
+              rib.handler(request, response); //数据接收完毕
+            });
+          }
         }
         catch {
           if (!response.finished) {
